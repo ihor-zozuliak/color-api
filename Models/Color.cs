18692828651924f785e04efc8c42427dbe328e5b
Ihor Zozuliak <ihor.zozuliak@avenga.com>
2020-01-23 12:50:23 +0200

    project files added

---
+++ Models/Color.cs
@@ -0,0 +1,8 @@
+namespace ColourAPI.Models
+{
+    public class Color
+    {
+        public int Id {get; set;}
+        public string ColorName{get; set;}
+    }
+}
---
+++ Models/ColorContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+namespace ColourAPI.Models
+{
+    public class ColorContext : DbContext
+    {
+        public ColorContext (DbContextOptions<ColorContext >options) : base(options)
+        {
+
+        }
+        public DbSet<Color> ColorItems{get; set;} 
+    }
+
+}
